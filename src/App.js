1c540faea2eb175397141ecd107e373499c96ebc
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-08-14 16:50:41 -0300

    routes e navbar:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  HomePage,
+  ProductsPage,
+  AboutPage,
+  SingleProductPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  //PrivateRoute,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/about">
+          <AboutPage />
+        </Route>
+        <Route exact path="/products">
+          <ProductsPage />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProductPage />} />
+        <Route exact path="/cart">
+          <CartPage />
+        </Route>
+        <Route exact path="/checkout">
+          <CheckoutPage />
+        </Route>
+        <Route patch="*">
+          <ErrorPage />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
