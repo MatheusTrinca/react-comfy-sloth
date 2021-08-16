d9dfe1182d6c01608d4cba6eb38effdd43f2efcb
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-08-16 12:44:18 -0300

    formspree

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero text="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="About" />
+        <article>
+          <div className="title">
+            <h2>Our History</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
+            mollitia optio est neque atque tenetur, ducimus quam temporibus
+            magnam dicta assumenda maiores soluta! Labore incidunt quasi, quis
+            eveniet officiis fugit non itaque consequatur sit laborum. Facere
+            aliquam sequi blanditiis dolor, repellat, voluptate laboriosam
+            sapiente praesentium porro ad explicabo aut rerum!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
