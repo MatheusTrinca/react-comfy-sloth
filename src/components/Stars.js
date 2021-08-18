35e7838c41d934595520434e0ed240dbfac245e6
MatheusTrinca <matheusfelipetrinca@gmail.com>
2021-08-18 16:24:04 -0300

    stars manually aproach

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from 'react';
+import styled from 'styled-components';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+const Stars = ({ stars, reviews }) => {
+  console.log(stars, reviews);
+  // <span>
+  //   {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}
+  // </span>; Ir repetindo mais 5x, acrescentando 2 e 1.5, 3 e 2.5, 4 e 3.5, 5 e 4.5
+  return (
+    <Wrapper>
+      <div className="stars"></div>
+      <p className="reviews">({reviews} customers reviews)</p>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -18,5 +27,5 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
   margin-bottom: 0.5rem;
-`
-export default Stars
+`;
+export default Stars;
