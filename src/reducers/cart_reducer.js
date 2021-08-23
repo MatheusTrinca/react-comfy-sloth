47ec667644481e8792ae25fbd72eed742bda3db2
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-08-23 12:05:34 -0300

    addToCart

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,49 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from '../actions';
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  switch (action.type) {
+    case ADD_TO_CART:
+      const { id, color, amount, product } = action.payload;
+      // o id sera o id + color, pois podem existir productos iguais e cores diferentes
+      const tempProduct = state.cart.find(p => p.id === id + color);
+      if (tempProduct) {
+        const tempCart = state.cart.map(cartItem => {
+          if (cartItem.id === id + color) {
+            let newAmount = cartItem.amount + amount;
+            if (newAmount > cartItem.max) {
+              newAmount = cartItem.max;
+            }
+            return { ...cartItem, amount: newAmount };
+          } else {
+            return cartItem;
+          }
+        });
+        return {
+          ...state,
+          cart: tempCart,
+        };
+      } else {
+        const newItem = {
+          id: id + color,
+          name: product.name,
+          color,
+          amount,
+          image: product.images[0].url,
+          price: product.price,
+          max: product.stock,
+        };
+        return {
+          ...state,
+          cart: [...state.cart, newItem],
+        };
+      }
 
-export default cart_reducer
+    default:
+      throw new Error(`No Matching "${action.type}" - action type`);
+  }
+};
+
+export default cart_reducer;
